7cfa91ddd48b11d87ebc896423d2e56d9343d94b
An Nguyễn <annt9934@gmail.com>
2022-04-26 21:30:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+$file = "text.txt";
+$fileOpen = fopen($file, "r");
+$arr = [];
+$arrHeadingOne = [];
+$arrHeadingTwo = [];
+$arrHeadingThree = [];
+$i = -1;
+$j = -1;
+
+while (!feof($fileOpen)) {
+    $contents = fgets($fileOpen, filesize($file));
+    $subStr = substr($contents, 0, 3);
+    $numberOfDash = substr_count($subStr, '-');
+    if ($numberOfDash == 0) {
+        $i++;
+        $arrHeadingOne[$i] = $contents;
+    } elseif ($numberOfDash == 2) {
+        $j++;
+        $arrHeadingTwo[$i][$j] = $contents;
+    } elseif ($numberOfDash == 3) {
+        $arrHeadingThree[$j][] = $contents;
+    }
+}
+fclose($fileOpen);
+
+foreach ($arrHeadingOne as $key => $arrayOne) {
+    $arr1 = [];
+    $arr2 = [];
+    $arr1['name'] = $arrayOne;
+    if (isset($arrHeadingTwo[$key])) {
+        foreach ($arrHeadingTwo[$key] as $key2 => $arrayTwo) {
+            $arr2['name'] = $arrayTwo;
+            $arr2['children'] = $arrHeadingThree[$key2] ?? [];
+            $arr1['children'][] = $arr2;
+        }
+    }
+
+    $arr[] = $arr1;
+}
+
+dd($arr);
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5f8e1096408c46ce38c8775fd0684f1d2dff28b3',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5f8e1096408c46ce38c8775fd0684f1d2dff28b3',
+    ),
+    'larapack/dd' => 
+    array (
+      'pretty_version' => '1.1',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '561b5111a13d0094b59b5c81b1572489485fb948',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.25.0',
+      'version' => '1.25.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0abb51d2f102e00a4eefcf46ba7fec406d245825',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.25.0',
+      'version' => '1.25.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4407588e0d3f1f52efb65fbe92babe41f37fe50c',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.4.6',
+      'version' => '5.4.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '294e9da6e2e0dd404e983daa5aa74253d92c05d0',
+    ),
+  ),
+);
